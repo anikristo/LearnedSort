389cf582445d59b126bc295b2c0ae95be73009f9
Ani Kristo <anikristo@gmail.com>
2020-04-30 21:56:47 -0400

    Delete quick-test.sh

--- quick-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-if [ ! -f "${DIR}/build/bin/LearnedSort_tests" ] 
-then 
-./compile.sh
-fi
-
-${DIR}/build/bin/LearnedSort_tests --gtest_shuffle --gtest_filter=*.StdNormal --test_size=1000000
