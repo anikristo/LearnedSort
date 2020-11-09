a8828942dd7b388720df386fd31580d300c9937c
Ani Kristo <anikristo@gmail.com>
2020-11-09 16:48:51 -0500

    Add new test case: UniformMod16

--- unit_tests/learned_sort_tests.cc
+++ unit_tests/learned_sort_tests.cc
@@ -215,3 +215,29 @@ TEST(LS_SORT_TEST, ReverseSorted) {
   // Test that it is sorted
   ASSERT_TRUE(std::is_sorted(arr.begin(), arr.end()));
 }
+
+/**
+ * Test Case suggested by @Morwenn: https://github.com/Morwenn
+ */
+TEST(LS_SORT_TEST, UniformMod16) {
+  
+  // Generate random array
+  std::vector<int> arr;
+  std::mt19937_64 prng(1604922353);
+  for (size_t i = 0; i < TEST_SIZE; ++i) {
+    arr.emplace_back(i % 16);
+  }
+  std::shuffle(arr.begin(), arr.end(), prng);
+
+  // Calculate the checksum
+  auto cksm = get_checksum(arr);
+
+  // Sort
+  learned_sort::sort(arr.begin(), arr.end());
+
+  // Test that the checksum is the same
+  ASSERT_EQ(cksm, get_checksum(arr));
+
+  // Test that it is sorted
+  ASSERT_TRUE(std::is_sorted(arr.begin(), arr.end()));
+}
