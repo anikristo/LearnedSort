ebe5e58f2f5845aa9d9d3c49ff483f9d4bbeaf2d
Ani Kristo <anikristo@gmail.com>
2021-07-02 12:42:38 -0400

    Lint parsers in data/

--- data/Stks/parser.py
+++ data/Stks/parser.py
@@ -8,12 +8,12 @@ DATASET_NAME = "Stks"
 COL_NAMES = ["Date", "Low", "Open", "Vol"]
 COL_DTYPES = [np.uint64, np.float64, np.float64, np.float64]
 
-for i in range(len(COL_NAMES)):
-    print("Processing {}/{}".format(DATASET_NAME, COL_NAMES[i]))
+for i, col_name in enumerate(COL_NAMES):
+    print("Processing {}/{}".format(DATASET_NAME, col_name))
 
     # Load data
     data = pd.read_csv(
-        "{}.txt".format(COL_NAMES[i]),
+        "{}.txt".format(col_name),
         dtype=COL_DTYPES[i],
         delimiter="\n",
         header=None,
@@ -28,13 +28,13 @@ for i in range(len(COL_NAMES)):
             unique_cnt, unique_cnt / data[0].shape[0]
         )
     )
-    with open("uniques_cnt_{}".format(COL_NAMES[i]), "w") as f:
+    with open("uniques_cnt_{}".format(col_name), "w") as f:
         f.write("{:d} ({:2.2%})".format(unique_cnt, unique_cnt / data[0].shape[0]))
 
     # Generate histograms
     plt.figure(figsize=(4, 4), dpi=144)
     data.hist(bins=50, grid=False)
-    plt.title("Distribution of {:s}/{:s}".format(DATASET_NAME, COL_NAMES[i]))
-    plt.savefig("hist_{:s}_{:s}.png".format(DATASET_NAME, COL_NAMES[i]))
+    plt.title("Distribution of {:s}/{:s}".format(DATASET_NAME, col_name))
+    plt.savefig("hist_{:s}_{:s}.png".format(DATASET_NAME, col_name))
     plt.clf()
     print("Histograms generated.")
